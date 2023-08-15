5c60f0e02a3cf95a4c4ac8722de3b9182542fc6b
prosper <edwardprosper001@gmail.com>
2023-08-15 23:15:13 +0100

    'new'

--- core/permissions.py
+++ core/permissions.py
@@ -41,6 +41,6 @@ class HasCompleteProfile(permissions.BasePermission):
         user = request.user
         if user.profile.is_employer:
             return True
-        self.message = "You must have a complete profile to create a post."
+        self.message = "You must be an employer to create a post."
         return False
 
--- core/views.py
+++ core/views.py
@@ -93,11 +93,17 @@ class ApplicationHomePage(ListCreateAPIView):
 
 class JobHomePage(ListCreateAPIView):
     queryset = Job.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly, HasCompleteProfile]
+    # permission_classes = [IsAuthenticatedOrReadOnly, HasCompleteProfile]
     serializer_class = JobSerializer
     search_fields = ["company_name", "title", "location"]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['company_name', 'expired']
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticatedOrReadOnly()]
+        else:
+            return [HasCompleteProfile()]
 
     def perform_create(self, serializer):
         serializer.save(posted_by=self.request.user)
